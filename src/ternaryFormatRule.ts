344515d9dce70d1a047da0e68b286d0710b7d762
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2018-11-18 09:37:23 +1300

    Added support for comments.

--- src/ternaryFormatRule.ts
+++ src/ternaryFormatRule.ts
@@ -28,11 +28,40 @@ interface ConditionalExpressionInfo {
   readonly colonToken: StartAndEndInfo;
   readonly whenFalse: StartAndEndInfo;
 }
+interface IdealConditionalExpression {
+  readonly condition: IdealPosition;
+  readonly questionToken: IdealPosition;
+  readonly whenTrue: IdealPosition;
+  readonly colonToken: IdealPosition;
+  readonly whenFalse: IdealPosition;
+}
 
 interface StartAndEndInfo {
   readonly start: ts.LineAndCharacter;
   readonly end: ts.LineAndCharacter;
 }
+
+interface IdealPosition {
+  readonly leadingComment?: string;
+  readonly trailingComment?: string;
+  readonly position: StartAndEndInfo;
+}
+
+type CommentInfo  =
+ | {
+      readonly comment: string;
+      readonly lines: number;
+      readonly pos: number;
+      readonly end: number;
+      readonly hasTrailingNewLine: boolean;
+    }
+  | {
+      readonly comment: undefined;
+      readonly lines: 0;
+      readonly pos: undefined;
+      readonly end: undefined;
+      readonly hasTrailingNewLine: false;
+    };
 
 const failureMessage = 'Incorrectly formatted ternary operator.';
 const INDENTATION = '  ';
@@ -50,7 +79,7 @@ function ruleEntryPoint(
   ctx: Lint.WalkContext<RuleOptions>
 ): RuleFunctionResult {
   if (isConditionalExpression(node)) {
-    const nodeInfo = getLineAndCharacterInfo(node, ctx.sourceFile);
+    const nodeInfo = getLineAndCharacterInfo(node);
     const trueFlowing = isConditionalExpression(node.whenTrue);
     const falseFlowing = isConditionalExpression(node.whenFalse);
 
@@ -90,33 +119,36 @@ function checkNonNested(
 
   const condition = getIdealConditionPosition(
     node,
-    ctx.sourceFile,
+    node.condition,
     nodeInfo.condition
   );
 
   const questionToken = getIdealTokenPosition(
-    condition.end.line,
-    condition.start.character
+    node.questionToken,
+    condition.position.end.line,
+    condition.position.start.character
   );
 
   const whenTrue = getIdealBranchPosition(
+    node.whenTrue,
     nodeInfo.whenTrue,
-    questionToken.end
+    questionToken.position.end
   );
 
   const colonToken = getIdealTokenPosition(
-    whenTrue.end.line,
-    condition.start.character
+    node.colonToken,
+    whenTrue.position.end.line,
+    condition.position.start.character
   );
 
   const whenFalse = getIdealBranchPosition(
+    node.whenFalse,
     nodeInfo.whenFalse,
-    colonToken.end
+    colonToken.position.end
   );
 
   return compareIdealToActual(
     node,
-    ctx.sourceFile,
     {
       condition,
       questionToken,
@@ -133,49 +165,89 @@ function checkNonNested(
  */
 function compareIdealToActual(
   node: ts.ConditionalExpression,
-  sourceFile: ts.SourceFile,
-  ideal: ConditionalExpressionInfo,
-  actual: ConditionalExpressionInfo
+  idealInfo: IdealConditionalExpression,
+  actualPosition: ConditionalExpressionInfo
 ): Array<InvalidNodeResult> {
-  if (deepEqual(ideal, actual, { strict: true })) {
+  if (
+    deepEqual(
+      {
+        condition: idealInfo.condition.position,
+        questionToken: idealInfo.questionToken.position,
+        whenTrue: idealInfo.whenTrue.position,
+        colonToken: idealInfo.colonToken.position,
+        whenFalse: idealInfo.whenFalse.position
+      },
+      actualPosition,
+      { strict: true }
+    )
+  ) {
     return [];
   }
 
-  const conditionLinesAbove = ideal.condition.start.line - actual.condition.end.line;
-  const conditionSpacesBefore = conditionLinesAbove > 0 ? ideal.condition.start.character : 0;
-  const questionTokenLinesAbove = ideal.questionToken.start.line - ideal.condition.end.line;
-  const questionTokenSpacesBefore = ideal.questionToken.start.character - (questionTokenLinesAbove === 0 ? conditionSpacesBefore + 1 : 0);
-  const whenTrueLinesAbove = ideal.whenTrue.start.line - ideal.questionToken.end.line;
-  const whenTrueSpacesBefore = ideal.whenTrue.start.character - (whenTrueLinesAbove === 0 ? questionTokenSpacesBefore + 1 : 0);
-  const colonTokenLinesAbove = ideal.colonToken.start.line - ideal.whenTrue.end.line;
-  const colonTokenSpacesBefore = ideal.colonToken.start.character - (colonTokenLinesAbove === 0 ? whenTrueSpacesBefore + 1 : 0);
-  const whenFalseLinesAbove = ideal.whenFalse.start.line - ideal.colonToken.end.line;
-  const whenFalseSpacesBefore = ideal.whenFalse.start.character - (whenFalseLinesAbove === 0 ? colonTokenSpacesBefore + 1 : 0);
+  const conditionLinesAbove =
+    idealInfo.condition.position.start.line - actualPosition.condition.end.line;
+  const conditionSpacesBefore =
+    conditionLinesAbove > 0 ? idealInfo.condition.position.start.character : 0;
+
+  const questionTokenLinesAbove =
+    idealInfo.questionToken.position.start.line - idealInfo.condition.position.end.line;
+  const questionTokenSpacesBefore =
+    idealInfo.questionToken.position.start.character -
+    (questionTokenLinesAbove === 0 ? conditionSpacesBefore + 1 : 0);
+
+  const colonTokenLinesAbove =
+    idealInfo.colonToken.position.start.line - idealInfo.whenTrue.position.end.line;
+  const colonTokenSpacesBefore =
+    idealInfo.colonToken.position.start.character - (colonTokenLinesAbove === 0 ? INDENTATION.length : 0);
+
+  const formatCode = (
+    code: string,
+    indent: number
+  ): string => {
+    return (
+      ' '.repeat(indent) + code
+    );
+  };
+
+  const formatComment = (
+    comment: string | undefined,
+    indent: number = 1,
+    trailingNewLine: boolean = false
+  ): string => {
+    return (
+      comment === undefined
+        ? ''
+        : (
+          ' '.repeat(indent) + comment.trim() + (trailingNewLine ? '\n' : '')
+        )
+    );
+  };
 
   return [markAsInvalidNode(node, failureMessage, [
     new Lint.Replacement(
-      ts.getPositionOfLineAndCharacter(
-        sourceFile,
-        actual.condition.start.line,
-        actual.condition.start.character
-      ),
-      node.getText().length,
+      node.getStart(),
+      node.getWidth(),
       (
-        '\n'.repeat(conditionLinesAbove) +
-        ' '.repeat(conditionSpacesBefore) +
-        node.condition.getText() +
-        '\n'.repeat(questionTokenLinesAbove) +
-        ' '.repeat(questionTokenSpacesBefore) +
-        node.questionToken.getText() +
-        '\n'.repeat(whenTrueLinesAbove) +
-        ' '.repeat(whenTrueSpacesBefore) +
-        node.whenTrue.getText() +
-        '\n'.repeat(colonTokenLinesAbove) +
-        ' '.repeat(colonTokenSpacesBefore) +
-        node.colonToken.getText() +
-        '\n'.repeat(whenFalseLinesAbove) +
-        ' '.repeat(whenFalseSpacesBefore) +
-        node.whenFalse.getText()
+        `${'\n'.repeat(conditionLinesAbove)
+        }${formatCode(node.condition.getText(), conditionSpacesBefore)
+        }${formatComment(idealInfo.condition.trailingComment)
+
+        }${'\n'
+        }${formatComment(idealInfo.questionToken.leadingComment, questionTokenSpacesBefore, true)
+        }${formatCode(node.questionToken.getText(), questionTokenSpacesBefore)
+
+        }${formatCode(node.whenTrue.getText(), 1)
+        }${formatComment(idealInfo.questionToken.trailingComment)
+        }${formatComment(idealInfo.whenTrue.leadingComment)
+        }${formatComment(idealInfo.whenTrue.trailingComment)
+
+        }${'\n'
+        }${formatComment(idealInfo.colonToken.leadingComment, colonTokenSpacesBefore, true)
+        }${formatCode(node.colonToken.getText(), colonTokenSpacesBefore)
+
+        }${formatCode(node.whenFalse.getText(), 1)
+        }${formatComment(idealInfo.colonToken.trailingComment)
+        }${formatComment(idealInfo.whenFalse.leadingComment)}`
       )
     )
   ])];
@@ -185,33 +257,43 @@ function compareIdealToActual(
  * Get the ideal position for the condition.
  */
 function getIdealConditionPosition(
-  node: ts.Node,
-  sourceFile: ts.SourceFile,
+  conditionalExpression: ts.ConditionalExpression,
+  node: ts.Expression,
   info: StartAndEndInfo
-): StartAndEndInfo {
+): IdealPosition {
   const conditionOnSameLine = isOnSameLineAsPreviousNode(
-    node,
-    info.start.line,
-    sourceFile
+    conditionalExpression,
+    info.start.line
   );
 
+  const {
+    hasTrailingNewLine: leadingCommentHasTrailingNewLine
+  } = getLeadingCommentInfo(node);
+
+  const {
+    comment: trailingComment
+  } = getTrailingCommentInfo(node);
+
   const indentAmount = getIndentationOfLine(
-    sourceFile,
-    info.start.line - (conditionOnSameLine ? 0 : 1)
+    info.start.line - (conditionOnSameLine ? 0 : 1),
+    node.getSourceFile()
   ) + (
-    isCommentAbove(node, sourceFile)
+    leadingCommentHasTrailingNewLine
       ? 0
       : INDENTATION.length
   );
 
   return {
-    start: {
-      line: info.start.line + (conditionOnSameLine ? 1 : 0),
-      character: indentAmount
-    },
-    end: {
-      line: info.end.line + (conditionOnSameLine ? 1 : 0),
-      character: indentAmount + info.end.character - info.start.character
+    trailingComment,
+    position: {
+      start: {
+        line: info.start.line + (conditionOnSameLine ? 1 : 0),
+        character: indentAmount
+      },
+      end: {
+        line: info.end.line + (conditionOnSameLine ? 1 : 0),
+        character: indentAmount + info.end.character - info.start.character
+      }
     }
   };
 }
@@ -220,17 +302,31 @@ function getIdealConditionPosition(
  * Get the ideal position for the question/colon token.
  */
 function getIdealTokenPosition(
+  node: ts.Token<ts.SyntaxKind.QuestionToken> | ts.Token<ts.SyntaxKind.ColonToken>,
   previousLine: number,
   conditionIndentation: number
-): StartAndEndInfo {
+): IdealPosition {
+  const {
+    comment: leadingComment,
+    lines: leadingCommentLines
+  } = getLeadingCommentInfo(node);
+
+  const {
+    comment: trailingComment
+  } = getTrailingCommentInfo(node);
+
   return {
-    start: {
-      line: previousLine + 1,
-      character: conditionIndentation + INDENTATION.length
-    },
-    end: {
-      line: previousLine + 1,
-      character: conditionIndentation + INDENTATION.length + 1
+    leadingComment,
+    trailingComment,
+    position: {
+      start: {
+        line: previousLine + leadingCommentLines + 1,
+        character: conditionIndentation + INDENTATION.length
+      },
+      end: {
+        line: previousLine + leadingCommentLines + 1,
+        character: conditionIndentation + INDENTATION.length + 1
+      }
     }
   };
 }
@@ -239,27 +335,41 @@ function getIdealTokenPosition(
  * Get the ideal position for a branch.
  */
 function getIdealBranchPosition(
+  node: ts.Expression,
   info: StartAndEndInfo,
   tokenPosition: ts.LineAndCharacter
-): StartAndEndInfo {
+): IdealPosition {
   const lineCount = (info.end.line - info.start.line) + 1;
 
+  const {
+    comment: leadingComment,
+    lines: leadingCommentLines
+  } = getLeadingCommentInfo(node);
+
+  const {
+    comment: trailingComment
+  } = getTrailingCommentInfo(node);
+
   return {
-    start: {
-      line: tokenPosition.line,
-      character: tokenPosition.character + 1
-    },
-    end: {
-      line: tokenPosition.line + lineCount - 1,
-      character:
-        lineCount === 1
-          ? (
-              tokenPosition.character +
-              info.end.character -
-              info.start.character +
-              1
-            )
-          : info.end.character
+    leadingComment,
+    trailingComment,
+    position: {
+      start: {
+        line: tokenPosition.line,
+        character: tokenPosition.character + leadingCommentLines + 1
+      },
+      end: {
+        line: tokenPosition.line + leadingCommentLines + lineCount - 1,
+        character:
+          lineCount === 1
+            ? (
+                tokenPosition.character +
+                info.end.character -
+                info.start.character +
+                1
+              )
+            : info.end.character
+      }
     }
   };
 }
@@ -268,15 +378,14 @@ function getIdealBranchPosition(
  * Get the line and character information for the given node.
  */
 function getLineAndCharacterInfo(
-  node: ts.ConditionalExpression,
-  sourceFile: ts.SourceFile
+  node: ts.ConditionalExpression
 ): ConditionalExpressionInfo {
   return {
-    condition: getStartAndEndInfo(node.condition, sourceFile),
-    questionToken: getStartAndEndInfo(node.questionToken, sourceFile),
-    whenTrue: getStartAndEndInfo(node.whenTrue, sourceFile),
-    colonToken: getStartAndEndInfo(node.colonToken, sourceFile),
-    whenFalse: getStartAndEndInfo(node.whenFalse, sourceFile)
+    condition: getStartAndEndInfo(node.condition),
+    questionToken: getStartAndEndInfo(node.questionToken),
+    whenTrue: getStartAndEndInfo(node.whenTrue),
+    colonToken: getStartAndEndInfo(node.colonToken),
+    whenFalse: getStartAndEndInfo(node.whenFalse)
   };
 }
 
@@ -285,14 +394,13 @@ function getLineAndCharacterInfo(
  */
 function isOnSameLineAsPreviousNode(
   node: ts.Node,
-  nodeLine: number,
-  sourceFile: ts.SourceFile
+  nodeLine: number
 ): boolean {
   const siblingBefore = getSiblingBefore(node);
   const siblingBeforeInfo =
     siblingBefore === undefined
       ? undefined
-      : getStartAndEndInfo(siblingBefore, sourceFile);
+      : getStartAndEndInfo(siblingBefore);
 
   return (
     siblingBeforeInfo === undefined
@@ -304,10 +412,9 @@ function isOnSameLineAsPreviousNode(
 /**
  * Get the position for the given node.
  */
-function getStartAndEndInfo(
-  node: ts.Node,
-  sourceFile: ts.SourceFile
-): StartAndEndInfo {
+function getStartAndEndInfo(node: ts.Node): StartAndEndInfo  {
+  const sourceFile = node.getSourceFile();
+
   return ({
     start: ts.getLineAndCharacterOfPosition(sourceFile, node.getStart()),
     end: ts.getLineAndCharacterOfPosition(sourceFile, node.getEnd())
@@ -315,32 +422,17 @@ function getStartAndEndInfo(
 }
 
 /**
- * Whether or not there is a comment above the given node (line-wise).
- */
-function isCommentAbove(
-  node: ts.Node,
-  sourceFile: ts.SourceFile
-): boolean {
-  const leadingCommentRange = ts.getLeadingCommentRanges(sourceFile.text, node.getFullStart());
-  return (
-    leadingCommentRange === undefined || leadingCommentRange.length === 0
-      ? false
-      : Boolean(leadingCommentRange[leadingCommentRange.length - 1].hasTrailingNewLine)
-  );
-}
-
-/**
  * Get the indentation of the given line.
  */
 function getIndentationOfLine(
-  sourceFile: ts.SourceFile,
-  indentationLine: number
+  lineNumber: number,
+  sourceFile: ts.SourceFile
 ): number {
   const lineRanges = getLineRanges(sourceFile);
-  if (indentationLine >= lineRanges.length || indentationLine < 0) {
+  if (lineNumber >= lineRanges.length || lineNumber < 0) {
     return 0;
   }
-  const lineRange = lineRanges[indentationLine];
+  const lineRange = lineRanges[lineNumber];
   const line = sourceFile.text.substring(lineRange.pos, lineRange.pos + lineRange.contentLength);
   const indentEnd = line.search(/\S/);
   return (
@@ -348,4 +440,63 @@ function getIndentationOfLine(
       ? 0
       : indentEnd
   );
+}
+
+/**
+ * Get the comment leading the given node.
+ */
+function getLeadingCommentInfo(node: ts.Node): CommentInfo {
+  const sourceFile = node.getSourceFile();
+
+  return getCommentInfo(
+    ts.getLeadingCommentRanges(sourceFile.text, node.getFullStart()),
+    sourceFile
+  );
+}
+
+/**
+ * Get the comment trailing the given node.
+ */
+function getTrailingCommentInfo(node: ts.Node): CommentInfo {
+  const sourceFile = node.getSourceFile();
+
+  return getCommentInfo(
+    ts.getTrailingCommentRanges(sourceFile.text, node.getEnd()),
+    sourceFile
+  );
+}
+
+/**
+ * Get the comment info from the given range.
+ */
+function getCommentInfo(
+  range: ReadonlyArray<ts.CommentRange> | undefined,
+  sourceFile: ts.SourceFile
+): CommentInfo {
+  if (range === undefined || range.length === 0) {
+    return {
+      comment: undefined,
+      lines: 0,
+      pos: undefined,
+      end: undefined,
+      hasTrailingNewLine: false
+    };
+  }
+
+  const hasTrailingNewLine = Boolean(range[range.length - 1].hasTrailingNewLine);
+
+  const comment = sourceFile.text.substring(
+      range[0].pos,
+      range[range.length - 1].end
+    ) + (hasTrailingNewLine ? '\n' : '');
+
+  const lines = range.length - 1 + (hasTrailingNewLine ? 1 : 0);
+
+  return {
+    comment,
+    lines,
+    pos: range[0].pos,
+    end: range[range.length - 1].end,
+    hasTrailingNewLine
+  };
 }
